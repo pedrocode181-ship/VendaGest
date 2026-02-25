e0b91bf64623d04f891fb74e1a920f82ed5fa3ad
pedrocode181-ship <pedrocode181@gmail.com>
2026-02-25 16:12:33 -0300

    Add files via upload

---
+++ grafico1.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import plotly.express as px 
+import streamlit as st 
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: linear-gradient(to right, #32CD32, #E3F2FD);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.title('Estrutura de vendas por cidade')
+tabela = pd.read_csv('vendas.csv')
+grafico = px.sunburst(tabela, path=['cidade', 'mercado'], values='valor', color='cidade',
+                     title='Estrutura de vendas ')
+
+st.plotly_chart(grafico)
+
+st.write('-> Click na cidade desejada para obter mais informações')
---
+++ login.py
@@ -0,0 +1,34 @@
+import streamlit as st 
+#cores
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: linear-gradient(to right, #32CD32, #E3F2FD);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+#login
+st.title('Seja bem vindo á VendaGest')
+st.write('## Faça o login')
+
+usuario = st.text_input('Digite seu nome: ')
+senha = st.text_input('Digite sua senha: ', type='password')
+
+if senha in ['1234', '4321', 'pedro']:
+    st.button('Entrar')
+    st.page_link('pages/principal.py',
+                 label='Entrar',
+                 icon='✔')
+    st.success('Ok, click no botão acima para avançar.')
+
+else:
+    st.error('Tente novamente.')  
+
+st.write('## Sobre a VendaGest')     
+st.write('-> A VendaGest é um site para você ter controle da sua empresa ou seu negocio.')
+st.write('Na VendaGest você tem acesso a graficos e tabelas de finaças da sua empresa.')
+st.write('Para ter mais informações acesse nosso instagram ou entre em contato com nossos desenvolvedores.')
